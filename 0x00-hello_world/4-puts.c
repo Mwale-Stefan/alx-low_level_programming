9e273fe9a2fac63d6f8a6fbc29a4029ee661d9ff
Mwale-Stefan <stefmwalegmail.com>
2022-09-09 05:45:02 -0700

    this better works

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-
-
 /**
- *
  * * main - print the string in the put function
  *
  * *
@@ -12,7 +9,7 @@
  *
  * * this program prints "Programming is like building a multilingual puzzle
  *
- * * Return: 0
+ * Return: 0
  *
  * */
 
@@ -20,8 +17,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
