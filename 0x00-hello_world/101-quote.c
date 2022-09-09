f66e4899bb833ad7eb46d9bc9a8d689fb2a0396e
Mwale-Stefan <stefmwalegmail.com>
2022-09-09 08:38:46 -0700

    UNIX genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 
 /**
  * main- prints a text
+ *
  * Return: Always one(success)
  */
-int manin(void)
+int main(void)
 {
 	write(2,and that piece of art is useful" - Dora Korpar, 2015-10-19\n",);
 	return(1);
