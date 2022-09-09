b1823ee0d47c28ef5fbe4001bd12cc97cbc45172
Mwale-Stefan <stefmwalegmail.com>
2022-09-09 12:52:50 -0700

    pos and neg

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.>
+#include <time.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+
+	if(n>0)
+	{printf("is positive %c\n");
+	}
+
+	else if (n==0)
+	{printf("is zero %c\n)");
+	}
+
+	else{
+		printf("is negative %c\n");
+	}
+
+	return (0);
+}
