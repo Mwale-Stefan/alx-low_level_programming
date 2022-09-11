c57d5a25220a632e29b78f66050d2775160469f2
Mwale-Stefan <stefmwalegmail.com>
2022-09-11 01:52:00 -0700

    print and exlude

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main:Print all the letters except q and e
+ *main - print all the letters except q and e
  *
- * Description: Using the main function
- * Print all the letters except q and e
+ *Description: Using the main function
+ *Print all the letters except q and e
  *Return:0
  */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 	if (ch != 'e' && ch != 'q')
