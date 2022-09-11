9f710f89e77b61a1ff1e1294205579eedb1fef10
Mwale-Stefan <stefmwalegmail.com>
2022-09-11 04:47:40 -0700

    comb of three

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - using main function
+ *
+ * Description: combine three
+ * three sumber combine fxn
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+			{
+			for (c = 2; c <= 9; c++)
+				{
+					if (c > b && b > a)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(c + '0');
+						if (a != 7)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+	}
+	putchar('\n');
+	return (0);
+}
