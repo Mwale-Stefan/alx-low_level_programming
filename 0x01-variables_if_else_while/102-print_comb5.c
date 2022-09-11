4c4fe1a8f4f40cdc63c77bafb235c4754f461281
Mwale-Stefan <stefmwalegmail.com>
2022-09-11 05:25:32 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - using main function
+ *
+ * Description: all combos of two numbers
+ * all possible combos
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 100 ; a++)
+	{
+		for (b + 0; b <= 100; b++)
+		{
+			if (b > a)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
