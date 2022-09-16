8663c0d3e65cd7af1010fa358bdffd30bda6f53c
Mwale-Stefan <stefmwalegmail.com>
2022-09-16 07:55:08 -0700

    upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _isupper(int c) - checks for uppercase character
+ * Return: 1 or 0 depending on condition
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' &&  c <= 'z')
+	{
+		_putchar(1);
+	}
+	else
+	{
+		_putchar(0);
+	}
+	_putchar('\n');
+	Return (0);
+}
