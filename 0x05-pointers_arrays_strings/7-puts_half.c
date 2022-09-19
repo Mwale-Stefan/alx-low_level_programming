b3388a27e6b8dea3a93a2eb7af9be2ad5f8a86e7
Mwale-Stefan <stefmwalegmail.com>
2022-09-19 11:15:31 -0700

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,11 +15,10 @@ void puts_half(char *str)
 	int len1 = len / 2;
 	int len2 = len - len1;
 	char *s1 = malloc(len1 + 1);
+	char *s2 = malloc(len2 + 1);
 
 	memcpy(s1, str, len1);
 	s1[len1] = '\0';
-	char *s2 = malloc(len2 + 1);
-
 	memcpy(s2, str + len1, len2);
 	s2[len2] = '\0';
 	free(s1);
